f310cd6b21e6439d8c6b737d68ae99841eabf5b8
gabrielanacleto-maker <gabriel.anacleto.cruz@gmail.com>
2026-08-11 14:33:32 -0300

    atualização 1

--- core/apps.py
+++ core/apps.py
@@ -1,16 +1,7 @@
 from django.apps import AppConfig
-import sys
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
     verbose_name = 'Modalidades'
-
-    def ready(self):
-        running_migrations = any(cmd in sys.argv for cmd in ['migrate', 'makemigrations'])
-        if running_migrations:
-            return
-        if 'RUN_MAIN' in sys.argv or sys.argv[1:2] == ['runserver']:
-            from core.scheduler import iniciar
-            iniciar()
